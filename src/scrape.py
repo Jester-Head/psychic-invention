586f0bb3697141b0af60adfddfceb8ad58406734
SB <19213803+Jester-Head@users.noreply.github.com>
2022-11-14 20:52:09 -0500

    Clicking links from class page

--- src/scrape.py
+++ src/scrape.py
@@ -14,55 +14,64 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 
 def get_class_page(driver,url):
-    driver.get(url)
-    # time.sleep(randint(1,5))
-    
-    author = WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR,'td.creator')))
-    author = [c.text for c in author]
-    
-    time.sleep(randint(1,5))
+    with driver:
+        driver.get(url)
+        time.sleep(randint(1,5))
+        
+        author = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR,'td.creator')))
+        author = [c.text for c in author]
+        
+        time.sleep(randint(1,5))
 
-    topic = driver.find_elements(By.CSS_SELECTOR,'td.main-link.clearfix')
-    topic = [t.text for t in topic]
-    
-    time.sleep(randint(1,5))
-
-
-    replies = driver.find_elements(By.CSS_SELECTOR,'td.num.posts-map.posts.topic-list-data > button > span')
-    replies = [r.text for r in replies]
-    
-    time.sleep(randint(1,5))
+        topic = driver.find_elements(By.CSS_SELECTOR,'td.main-link.clearfix')
+        topic = [t.text for t in topic]
+        
+        time.sleep(randint(1,5))
 
 
-    views = driver.find_elements(By.CSS_SELECTOR,'td.num.views > span')
-    views = [v.text for v in views]
-    
-    time.sleep(randint(1,5))
+        replies = driver.find_elements(By.CSS_SELECTOR,'td.num.posts-map.posts.topic-list-data > button > span')
+        replies = [r.text for r in replies]
+        
+        time.sleep(randint(1,5))
 
-    link = driver.find_elements(By.CSS_SELECTOR,'td.main-link.clearfix > span > a')
-    link = [l.get_attribute('href') for l in link]
+
+        views = driver.find_elements(By.CSS_SELECTOR,'td.num.views > span')
+        views = [v.text for v in views]
+        
+        time.sleep(randint(1,5))
+
+        link = driver.find_elements(By.CSS_SELECTOR,'td.main-link.clearfix > span > a')
+        link = [l.get_attribute('href') for l in link]
+        
+        time.sleep(2)
+        wow_class = driver.find_element(By.CSS_SELECTOR,'#ember49-header .category-name')
     # driver.quit()
 
     df = pd.DataFrame(list(zip(topic,author,views,replies)),columns=['topic','author','views','replies'])
+    
+    df['class'] = wow_class.text 
     return df
 
 def get_replies(driver,url):
     with driver:
         driver.get(url)
-        # comments = driver.find_elements(By.CSS_SELECTOR,'div.topic-body.clearfix.highlighted > div.regular.contents')
-        # comments = WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR,'class.post[itemprop="articleBody"]')))
-        # comments = driver.find_elements(By.XPATH,'.//*[contains(concat( " ", @class, " " ), concat( " ", "post-stream", " " ))]')
+        class_forum = 'Demon Hunter'
         comments = driver.find_elements(By.XPATH,'.//*[contains(concat( " ", @class, " " ), concat( " ", "cooked", " " ))]')
         comments = [com.text for com in comments]
-        # comments = comments[3:]
-        # time.sleep(randint(1,5))
-        # author = driver.find_elements(By.XPATH,'.//*[contains(concat( " ", @class, " " ), concat( " ", "character-name", " " ))]')
-        # author = [a.text for a in author]
-        
-        # df = pd.DataFrame(list(zip(comments,author)),columns=['comments','author'])
-        df = pd.DataFrame(comments,columns=['comments'])
+        df = pd.DataFrame(data=comments,columns=['comments'])
+        df['class'] = class_forum
 
     return df
+
+def get_links(driver,url):
+    with driver:
+        driver.get(url)
+        time.sleep(randint(1,5))
+        link = driver.find_elements(By.CSS_SELECTOR,'td.main-link.clearfix > span > a')
+    class_links = [l.get_attribute('href') for l in link]
+        # class_links = [item.get_attribute("href") for item in wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,"td.main-link.clearfix > span > a")))]
+
+    return class_links
     
 
 
@@ -86,22 +95,18 @@ if __name__== "__main__":
     options.add_argument('--no-first-run --no-service-autorun --password-store=basic')
     driver = uc.Chrome(options=options)
     
-    
-    # service = Service('C:\chromedriver_win32\chromedriver')
-    # driver = webdriver.Chrome('C:\chromedriver_win32\chromedriver')
+
     url = 'https://us.forums.blizzard.com/en/wow/c/classes/death-knight/175'
     # url = 'https://us.forums.blizzard.com/en/wow/t/please-fix-momentum-before-launch-day/1381495'
 #  
     # dfc = get_replies(driver,url)
-    dfc = get_class_page(driver=driver,url=url)
+    # dfc = get_class_page(driver=driver,url=url)
     # print(dfc)
-    # dfc.to_csv('output',index=False)
-    dfc.to_csv('class_page',index=False)
+    # dfc.to_csv('class_threads',index=False)
+    # dfc.to_csv('class_page',index=False)
+    links = get_links(driver,url)
+    # print(links)
+    post = get_replies(driver,links[0])
     driver.quit()
+    print(post)
 
-# for next page
-# <div role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement" class="topic-body crawler-post">
-
-# body > div.optanon-alert-box-wrapper.hide-cookie-setting-button > div.optanon-alert-box-bg > div.optanon-alert-box-button-container > div.optanon-alert-box-button.optanon-button-allow > div > button
-
-# <button class="optanon-allow-all accept-cookies-button" title="Accept cookies" aria-label="Accept cookies" onclick="Optanon.TriggerGoogleAnalyticsEvent('OneTrust Cookie Consent', 'Banner Accept Cookies');" tabindex="4">Accept cookies</button>
