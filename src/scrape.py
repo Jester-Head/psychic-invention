f8c81d8e649f44a2328581c1f69221cd1d712ac0
SB <19213803+Jester-Head@users.noreply.github.com>
2022-11-19 19:44:11 -0500

    Extracting comment dates

--- src/scrape.py
+++ src/scrape.py
@@ -9,8 +9,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+import os
 
-# import no_detect
 
 
 def get_class_page(driver,url):
@@ -45,22 +45,32 @@ def get_class_page(driver,url):
         
         time.sleep(2)
         wow_class = driver.find_element(By.CSS_SELECTOR,'#ember49-header .category-name')
-    # driver.quit()
 
     df = pd.DataFrame(list(zip(topic,author,views,replies)),columns=['topic','author','views','replies'])
     
     df['class'] = wow_class.text 
     return df
 
-def get_replies(driver,url):
+def get_replies(driver,url,class_name,count=1):
+    # comment=[]
     with driver:
         driver.get(url)
-        class_forum = 'Demon Hunter'
-        comments = driver.find_elements(By.XPATH,'.//*[contains(concat( " ", @class, " " ), concat( " ", "cooked", " " ))]')
+    #     for reply in range(1,int(count)):
+    #         selector = f'#post_{reply} .cooked'
+    #         comment.append(driver.find_element(By.CSS_SELECTOR,selector).text)
+            
+            # comments = [com.text for com in comments]
+            # df = pd.DataFrame(columns=['comment'])
+            # df['comment'][reply] = comment.text
+            
+        comments = driver.find_elements(By.CSS_SELECTOR,'.cooked > p')
         comments = [com.text for com in comments]
-        df = pd.DataFrame(data=comments,columns=['comments'])
-        df['class'] = class_forum
-
+        time.sleep(randint(1,5))
+        dates = driver.find_elements(By.CSS_SELECTOR,'.post-info.post-date > a > span')
+        dates = [date.get_attribute('title') for date in dates]
+        df = pd.DataFrame(list(zip(comments, dates)),
+            columns =['comment', 'date'])        
+        df['class'] = class_name
     return df
 
 def get_links(driver,url):
@@ -69,13 +79,37 @@ def get_links(driver,url):
         time.sleep(randint(1,5))
         link = driver.find_elements(By.CSS_SELECTOR,'td.main-link.clearfix > span > a')
     class_links = [l.get_attribute('href') for l in link]
-        # class_links = [item.get_attribute("href") for item in wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,"td.main-link.clearfix > span > a")))]
-
     return class_links
+
+def check_file(class_name):
+    filename = 'data\\'+ class_name +'.csv'
+    if not os.path.exists(filename):
+        with open(filename, 'w',encoding='utf-8') as f:
+            f.writelines(['comment,date,class\n'])
+    else:
+        with open (filename,'r+',encoding='utf-8') as f:
+            
+            if str(f.readline())=='':
+                f.writelines(['comment,date,class\n'])
+    return filename
     
+def scrape_class(driver,links,class_name,count=1):
+    filename = check_file(class_name)
+    for link in links:
+        time.sleep(randint(1,5))
+        df = get_replies(driver=driver,url=link,class_name=class_name,count=count)
+        df.to_csv(filename,mode='a',index=False,header=False)
+        
+def get_reply_count(driver,url):
+    
+    with driver:
+        driver.get(url)
+        time.sleep(randint(1,5))
+        count = driver.find_element(By.CSS_SELECTOR,'.replies>span').text
 
-
-if __name__== "__main__":
+    return count
+               
+def avoid_detection():
     options = uc.ChromeOptions()
 
     # setting profile
@@ -94,19 +128,33 @@ if __name__== "__main__":
     # just some options passing in to skip annoying popups
     options.add_argument('--no-first-run --no-service-autorun --password-store=basic')
     driver = uc.Chrome(options=options)
+    return driver
+        
     
 
-    url = 'https://us.forums.blizzard.com/en/wow/c/classes/death-knight/175'
-    # url = 'https://us.forums.blizzard.com/en/wow/t/please-fix-momentum-before-launch-day/1381495'
-#  
-    # dfc = get_replies(driver,url)
-    # dfc = get_class_page(driver=driver,url=url)
-    # print(dfc)
-    # dfc.to_csv('class_threads',index=False)
-    # dfc.to_csv('class_page',index=False)
-    links = get_links(driver,url)
-    # print(links)
-    post = get_replies(driver,links[0])
-    driver.quit()
-    print(post)
 
+if __name__== "__main__":
+    driver = avoid_detection()
+    # Add Evoker later
+    classes = {'Death Knight':'https://us.forums.blizzard.com/en/wow/c/classes/death-knight/175','Demon Hunter':'https://us.forums.blizzard.com/en/wow/c/classes/demon-hunter/176','Druid':'https://us.forums.blizzard.com/en/wow/c/classes/druid/177',
+               'Hunter':'https://us.forums.blizzard.com/en/wow/c/classes/hunter/178','Mage':'https://us.forums.blizzard.com/en/wow/c/classes/mage/179','Monk':'https://us.forums.blizzard.com/en/wow/c/classes/monk/180',
+               'Paladin':'https://us.forums.blizzard.com/en/wow/c/classes/paladin/181','Priest':'https://us.forums.blizzard.com/en/wow/c/classes/priest/182','Rogue':'https://us.forums.blizzard.com/en/wow/c/classes/rogue/183',
+               'Shaman':'https://us.forums.blizzard.com/en/wow/c/classes/shaman/184','Warlock':'https://us.forums.blizzard.com/en/wow/c/classes/warlock/185','Warrior':'https://us.forums.blizzard.com/en/wow/c/classes/warrior/186'}
+    # classes = {'Death Knight':'https://us.forums.blizzard.com/en/wow/c/classes/death-knight/175'}
+    for name in classes:
+        url = classes.get(name)
+        links = get_links(driver,url)
+        scrape_class(driver=driver,links=links,class_name=name)
+        time.sleep(randint(1,5))
+    
+    # url = 'https://us.forums.blizzard.com/en/wow/t/best-dk-names-thread-the-good-the-great-and-the-punny/698565'
+    # count = get_reply_count(driver=driver,url=url)
+    # # selector = f'#post_{count} > div > div.topic-body.clearfix > div.regular.contents > div'
+    # # print(selector)
+    # # df = get_replies(driver=driver,url=url,class_name='Death Knight',count=count)
+    # print(df)
+
+    driver.quit()
+   
+    
+    
